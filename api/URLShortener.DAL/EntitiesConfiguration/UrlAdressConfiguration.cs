7105206ff7fd72b21fbd968e08dc6b90018d8ae7
botrkl <96987652+botrkl@users.noreply.github.com>
2024-05-25 18:46:10 +0300

    Implmented EntitiesConfiguration

---
+++ api/URLShortener.DAL/EntitiesConfiguration/UrlAdressConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using URLShortener.DAL.Entities;
+
+namespace URLShortener.DAL.EntitiesConfiguration
+{
+    public class UrlAdressConfiguration : IEntityTypeConfiguration<UrlAdress>
+    {
+        public void Configure(EntityTypeBuilder<UrlAdress> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.OriginalUrl)
+                .IsRequired();
+
+            builder.Property(x => x.ShortenedUrl)
+                .IsRequired();
+
+            builder.Property(x=>x.CreatedDate)
+                .IsRequired();
+
+            builder.Property(x => x.UserId)
+                .IsRequired();
+        }
+    }
+}
---
+++ api/URLShortener.DAL/EntitiesConfiguration/UserConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using URLShortener.DAL.Entities;
+
+namespace URLShortener.DAL.EntitiesConfiguration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.HasIndex(x => x.Username)
+                .IsUnique();
+            builder.Property(x => x.Username)
+                .IsRequired();
+
+            builder.Property(x => x.HashPassword)
+                .IsRequired();
+
+            builder.Property(x => x.IsAdmin)
+                .IsRequired()
+                .HasDefaultValue(false);
+
+            builder.HasMany(x => x.UrlAdresses)
+                .WithOne(url => url.CreatedBy)
+                .HasForeignKey(url => url.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
