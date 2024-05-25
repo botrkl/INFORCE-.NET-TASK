a8bafe76cfd94872015fad3bafebe6e293a57e29
botrkl <96987652+botrkl@users.noreply.github.com>
2024-05-25 23:38:41 +0300

    Implemented PasswordHashingService

---
+++ api/URLShortener.BLL/Services/Classes/PasswordHashingService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+using URLShortener.BLL.Services.Intefaces;
+
+namespace URLShortener.BLL.Services.Classes
+{
+    public class PasswordHashingService : IPasswordHashingService
+    {
+        public async Task<string> HashPasswordAsync(string password)
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                await Task.Run(() => rng.GetBytes(salt));
+            }
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8));
+
+            return $"{Convert.ToBase64String(salt)}:{hashed}";
+        }
+
+        public Task<bool> VerifyPasswordAsync(string password, string hashedPassword)
+        {
+            string[] parts = hashedPassword.Split(':');
+            byte[] salt = Convert.FromBase64String(parts[0]);
+
+            string hashedInputPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8));
+
+            return Task.FromResult(hashedInputPassword == parts[1]);
+        }
+    }
+}
---
+++ api/URLShortener.BLL/Services/Intefaces/IPasswordHashingService.cs
@@ -0,0 +1,8 @@
+﻿namespace URLShortener.BLL.Services.Intefaces
+{
+    public interface IPasswordHashingService
+    {
+        Task<string> HashPasswordAsync(string password);
+        Task<bool> VerifyPasswordAsync(string password, string hashedPassword);
+    }
+}
