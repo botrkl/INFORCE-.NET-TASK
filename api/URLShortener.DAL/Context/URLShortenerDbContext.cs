301f771d9dfe9a918929bc1d0e50b644401ba687
botrkl <96987652+botrkl@users.noreply.github.com>
2024-05-25 18:49:11 +0300

    Added DbContext

---
+++ api/URLShortener.DAL/Context/URLShortenerDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using URLShortener.DAL.Entities;
+using URLShortener.DAL.EntitiesConfiguration;
+
+namespace URLShortener.DAL.Context
+{
+    public class URLShortenerDbContext : DbContext
+    {
+        public URLShortenerDbContext(DbContextOptions<URLShortenerDbContext> options) : base(options){}
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UrlAdress> UrlAdresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new UrlAdressConfiguration());
+        }
+    }
+}
